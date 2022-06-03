3e2da03ebc9e2a3a9b2483eaba84dfce75f4bb6f
Otto Jones <ottosjonesjr@gmail.com>
2022-06-03 19:39:16 +0300

    added addtional route

---
+++ petfax/fact.py
@@ -0,0 +1,16 @@
+from flask import (Blueprint, render_template)
+
+bp = Blueprint('fact', __name__, url_prefix='/facts')
+
+
+@bp.route('/')
+def new():
+    return render_template('facts/new.html')
+
+
+@bp.route('/new')
+def new():
+    return render_template('facts/new.html')
+
+
+bp = Blueprint('fact', __name__, url_prefix='/facts')
