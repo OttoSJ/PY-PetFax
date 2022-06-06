ae158ae57e730c2c23f078d3de8aab838755c3aa
Otto Jones <ottosjonesjr@gmail.com>
2022-06-06 10:38:18 +0300

    added database

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_migrate import Migrate
+from . import models
 
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Rookie121215@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
 
     @app.route('/')
     def hello():
@@ -10,9 +18,8 @@ def create_app():
 
     from . import pet
     app.register_blueprint(pet.bp)
+
     from . import fact
     app.register_blueprint(fact.bp)
-    return app
 
-    app.config['SQLALCHENY_DATABASE_URL'] = 'postgres://postgress:Rookie121215@localhost:5432/petfax'
-    app.config['SQLALCHENY_TRACK_MODIFICATIONS'] = False
+    return app
--- petfax/models.py
+++ petfax/models.py
@@ -1,3 +1,12 @@
+# from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+class Fact(db.Model):
+    __tablename__ = 'facts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    submitter = db.Column(db.String(250))
+    fact = db.Column(db.Text)
